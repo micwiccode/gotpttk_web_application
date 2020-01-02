972ec7a49a9eaa0ca73fc06b0b3746c993fc98f8
Michał <m.wiczk@wp.pl>
2020-01-02 16:34:31 +0100

    Add findSection view

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -6,12 +6,26 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class CreateRouteController extends AbstractController{
+class RouteController extends AbstractController{
   /**
    * @Route("/createRoute")
    */
   public function index(){
     $routes = ["Point1", "Point2"];
     return $this->render('createRoute.html.twig', array('routes' => $routes));
+  }
+
+  /**
+   * @Route("/findSection")
+   */
+  public function findSection(){
+    return $this->render('findSection.html.twig');
+  }
+
+  /**
+   * @Route("/createOwnSection")
+   */
+  public function createOwnSection(){
+    return $this->render('createOwnSection.html.twig');
   }
 }
