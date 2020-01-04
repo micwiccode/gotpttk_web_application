982ea3e6d1a728003a7dcf42a7a019162b8da6e2
Szymon-G <s.gasior.sw@wp.pl>
2020-01-04 22:39:03 +0100

    Login verification with hardcoded values

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class LoginController extends AbstractController
@@ -23,4 +25,40 @@ class LoginController extends AbstractController
   {
     return $this->render('resetPassword.html.twig');
   }
+
+  /**
+   * @Route("/verifyLogin", methods={"POST"})
+   */
+  public function verifyLogin()
+  {
+    $request = Request::createFromGlobals();
+    $bEmail = false;
+    $bPasswcord = false;
+    
+    //check email
+    if($request->request->get('email') == 'user@user.com'){
+      $bEmail = true;
+      //check password
+      if($request->request->get('password') == '1234'){
+        $bPassword = true;
+        return $this->render('book.html.twig');  
+      }
+      else{
+        $this->addFlash(
+          'password',
+          'visible'
+        );
+      }
+    }
+    else{
+      $this->addFlash(
+        'email',
+        'visible'
+      );
+    }
+    return $this->render('login.html.twig');
+    //return new Response('<html><body>'.$email.'</body></html>');
+  }
+
+
 }
